f6695dd74c9fcd3f774360c366352ec439ef90df
Valyrn <valhernr@gmail.com>
2022-12-01 09:46:46 -0600

    CharacterDND5 added details, @OnetoMany to new db DnDEquipment

--- src/main/kotlin/com/github/valhryn/rpgbot/systemCharacters/CharacterDND5.kt
+++ src/main/kotlin/com/github/valhryn/rpgbot/systemCharacters/CharacterDND5.kt
@@ -1,16 +1,20 @@
 package com.github.valhryn.rpgbot.systemCharacters
 
 import com.github.valhryn.rpgbot.Campaign
+import com.github.valhryn.rpgbot.equipment.DnDEquipment
 import com.github.valhryn.rpgbot.domain.Base
-import dev.kord.common.entity.optional.optional
 import javax.persistence.Entity
 import javax.persistence.ManyToOne
+import javax.persistence.OneToMany
 
 @Entity
-class CharacterDND5(name : String) : Base() {
+class CharacterDND5(name : String, race : String, job : String, abilityScores : IntArray) : Base() {
 
     var name: String = name
 
     @ManyToOne(optional = false)
     lateinit var campaign : Campaign
+
+    @OneToMany(mappedBy = "CharacterDND5")
+    lateinit var items : DnDEquipment
 }
